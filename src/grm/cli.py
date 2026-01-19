c1c3f00d627481ceced4384b448679ca906c6b6c
Youngwoo Kim <youngwoo.kim.200@kt.com>
2026-01-19 17:26:35 +0900

    feat: impl ls

--- src/grm/cli.py
+++ src/grm/cli.py
@@ -3,6 +3,7 @@ import sys
 from .config import parser as config_parser, cmd_config, configure, config
 from .exceptions import CommandExit
 from .find import parser as find_parser, cmd_find
+from .ls import parser as ls_parser, cmd_ls
 from .options import parser
 
 
@@ -11,6 +12,7 @@ log = logging.getLogger(__name__)
 parser.set_defaults(func=None)
 config_parser.set_defaults(func=cmd_config)
 find_parser.set_defaults(func=cmd_find)
+ls_parser.set_defaults(func=cmd_ls)
 
 
 def main():
---
+++ src/grm/ls.py
@@ -0,0 +1,36 @@
+import argparse
+
+from pathlib import Path
+
+from .find import find_repos
+from .options import subparsers, render_parser
+from .render import render_repos
+
+
+parser = subparsers.add_parser(
+    "ls",
+    parents=[render_parser],
+    description="List repositories in a directory",
+    help="List repositories in a directory",
+)
+
+parser.add_argument(
+    "path",
+    nargs='?',
+    default=Path.cwd(),
+    type=Path,
+    help="directory path to search",
+)
+
+parser.add_argument(
+    "-a",
+    action="store_const",
+    const=True,
+    help="alias for '--hidden'",
+    dest="hidden",
+)
+
+
+def cmd_ls(args: argparse.Namespace):
+    repos = find_repos(args.path, max_depth=1)
+    render_repos(repos, args)
